c1de80f58dba4bbdf449f2d125b5580956d8dc3c
Lenovo <Lenovo@RohAn>
2017-05-13 13:58:05 +0530

    commited

---
+++ src/java/Lematize.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import edu.stanford.nlp.ling.CoreAnnotations.LemmaAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author aayus
+ */
+
+public class Lematize extends HttpServlet {
+protected StanfordCoreNLP pipeline;
+//            public Lematize() {
+//    // Create StanfordCoreNLP object properties, with POS tagging
+//    // (required for lemmatization), and lemmatization
+//  
+//}
+
+public List<String> lemmatize(String documentText)
+{
+      Properties props;
+    props = new Properties();
+    props.put("annotators", "tokenize, ssplit, pos, lemma");
+
+    
+    this.pipeline = new StanfordCoreNLP(props);
+    List<String> lemmas = new LinkedList<String>();
+    // Create an empty Annotation just with the given textd
+    Annotation document = new Annotation(documentText);
+    // run all Annotators on this text
+    this.pipeline.annotate(document);
+    // Iterate over all of the sentences found
+    List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+    for(CoreMap sentence: sentences) {
+        // Iterate over all tokens in a sentence
+        for (CoreLabel token: sentence.get(TokensAnnotation.class)) {
+            // Retrieve and add the lemma for each word into the
+            // list of lemmas
+            lemmas.add(token.get(LemmaAnnotation.class));
+        }
+    }
+    return lemmas;
+}
+
+protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+         response.setContentType("text/html");
+         PrintWriter out = response.getWriter();
+         String a =request.getParameter("firstname");
+
+
+//public static void main(String[] args) {
+
+    //out.println("Starting Stanford Lemmatizer");
+   //String text = "Fuckers ";
+  
+    Lematize slem = new Lematize();
+    out.println(slem.lemmatize(a));
+}
+}
+        
+    
+    
+    
+
